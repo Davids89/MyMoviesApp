de800c24a26907b4b09dde2b75c1a321a73a7118
David Luque <dluque@vectorsf.com>
2016-02-10 11:34:10 +0100

    Implementing Gulp to launch server and browser

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,31 @@
 var gulp = require('gulp');
 var server = require('gulp-express');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('server', function(){
+gulp.task('nodemon', function(cb){
 
-    server.run(['server.js']);
+    console.log(cb);
 
+    var started = false;
+
+    return nodemon({
+        script : 'server.js'
+    }).on('start', function(){
+        if(!started){
+            cb();
+            started = true;
+        }
+    })
 });
+
+gulp.task('browser-sync', ['nodemon'], function(){
+   browserSync.init(null, {
+       proxy: 'localhost:8080',
+       files : ["public/**/*.*"],
+       browser : 'google chrome',
+       port : 8080
+   });
+});
+
+gulp.task('default', ['browser-sync']);
