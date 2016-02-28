3ec529234b48b890ff072c41e640c8af877adee7
David Luque <daviluqui@gmail.com>
2016-02-28 11:58:55 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,71 @@
+/*
+    David Luque Quintana
+ */
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var flash = require('connect-flash');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var database = require('./config/database.js');
+var http = require('http');
+var CronJob = require('cron').CronJob;
+var passport = require('passport');
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+mongoose.connect(database.url);
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+//app.set('view engine', 'ejs');
+
+app.use(session({ secret : 'ilovescotchscotchyscotchscotch' }));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var config = getConfiguration();
+
+require('./app/routes/routes.js')(app, passport);
+require('./app/routes/session.js')(app, passport);
+
+//api files
+
+//cinema file
+//require('./config/addCinemas.js');
+
+//task
+new CronJob('00 24 16 * * 3', function(){
+    require('./config/popularMovies.js');
+}, null, true, 'Europe/Madrid');
+
+app.listen(port);
+console.log('Servidor funcionando en el puerto ' + port);
+
+function getConfiguration(){
+    http.get('http://api.themoviedb.org/3/configuration?api_key=7c45e91d96f141e78609a00969329847', function(resp){
+        var data = '';
+
+        resp.on('data', function(chunk){
+            data += chunk;
+        });
+
+        resp.on('end', function(){
+            try{
+                require('./app/api/movies_api.js')(app, data);
+                require('./app/api/genres_api.js')(app, data);
+            }catch(e){
+                console.log(e);
+            }
+        })
+    });
+}
