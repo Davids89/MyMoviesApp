c96591a9423d6f06aa54e0a0826faf56aaeb68f3
David Luque <dluque@vectorsf.com>
2016-04-18 17:30:34 +0200

    Adding test to a factory

--- public/app.js
+++
@@ -1,9 +0,0 @@
-define([
-    'angular',
-    'routes'
-], function(angular, routes){
-    'use strict';
-
-    var app = angular.module('app', []);
-    app.config(routes);
-});
---
+++ tests/spec/factories/Movie.spec.js
@@ -0,0 +1,12 @@
+describe('Testing Movie model', function(){
+
+    var factory;
+
+    beforeEach(angular.mock.module('app'));
+
+    beforeEach(angular.mock.inject(function(Movie){
+        factory = Movie;
+    }));
+
+
+});
